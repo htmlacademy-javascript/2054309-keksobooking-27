02d502156393b06016d3a1dc4734d51a74d27f65
Nikita <lisenkovandrei@yandex.ru>
2022-12-04 01:21:18 +0400

    8.22. Правда или действие (часть 2) modified:   js/validate.js

--- js/validate.js
+++ js/validate.js
@@ -3,25 +3,49 @@ const offerInformationForm = document.querySelector('.ad-form');
 const MIN_SYMBOLS_VALUE = 30;
 const MAX_SYMBOLS_VALUE = 100;
 const MAX_PRICE = 100000;
+const MIN_PRICES_FOR_TYPES = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
+};
 
+const typeField = document.querySelector('#type');
+const timeInField = document.querySelector('#timein');
+const timeOutField = document.querySelector('#timeout');
 const titleField = offerInformationForm.querySelector('#title');
 const priceField = offerInformationForm.querySelector('#price');
 const roomsField = offerInformationForm.querySelector('#room_number');
 const capacityField = offerInformationForm.querySelector('#capacity');
 
+const validateTime = () => {
+  timeOutField.value = timeInField.value;
+  return timeInField.value === timeOutField.value;
+};
+
+const getPriceErrorMessage = () => `От ${MIN_PRICES_FOR_TYPES[typeField.value]} руб. до ${MAX_PRICE} руб.`;
+
 const validateTitleField = (value) => value.length >= MIN_SYMBOLS_VALUE && value.length <= MAX_SYMBOLS_VALUE;
-const validatePriceField = (value) => value <= MAX_PRICE;
+const validatePriceField = (value) => value <= MAX_PRICE && MIN_PRICES_FOR_TYPES[typeField.value] <= value;
 const validateCapacityField = () => roomsField.value === '100' ? capacityField.value === '0' : roomsField.value >= capacityField.value && capacityField.value !== '0';
 
 const initValidateData = () => {
-  const getCapacityErrorMessage = () => {
+
+  const getRoomsErrorMessage = () => {
     if (roomsField.value === '100') {
       return 'Не для гостей';
     }
-    if (capacityField.value === '0') {
-      return 'Только для помещений, имеющих 100 комнат!';
+    if (roomsField.value === '3') {
+      return 'Должен быть хотя бы 1 гость!';
     }
-    return `Должно быть как минимум ${capacityField.value} комнаты.`;
+    if (roomsField.value === '2') {
+      return 'Должно быть 1 или 2 гостя!';
+    }
+    if (roomsField.value === '1') {
+      return 'Должен быть ровно 1 гость!';
+    }
+
   };
 
   const pristine = new Pristine(offerInformationForm, {
@@ -34,9 +58,23 @@ const initValidateData = () => {
   });
 
   pristine.addValidator(titleField, validateTitleField, `Введите от ${MIN_SYMBOLS_VALUE} до ${MAX_SYMBOLS_VALUE} символов`);
-  pristine.addValidator(priceField, validatePriceField, `Цена должна быть не более ${MAX_PRICE} руб.`);
-  pristine.addValidator(capacityField, validateCapacityField, getCapacityErrorMessage);
-  pristine.addValidator(roomsField, validateCapacityField, getCapacityErrorMessage);
+  pristine.addValidator(priceField, validatePriceField, getPriceErrorMessage);
+  pristine.addValidator(capacityField, validateCapacityField, getRoomsErrorMessage);
+  pristine.addValidator(timeInField, validateTime);
+  pristine.addValidator(timeOutField, validateTime);
+
+  typeField.addEventListener('change', () => {
+    priceField.placeholder = MIN_PRICES_FOR_TYPES[typeField.value];
+    pristine.validate(priceField);
+  });
+
+  capacityField.addEventListener('change', () => {
+    pristine.validate(capacityField);
+  });
+
+  roomsField.addEventListener('change', () => {
+    pristine.validate(capacityField);
+  });
 
   offerInformationForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
